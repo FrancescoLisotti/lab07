e6d9d916ab719dc96f256c4d2cd5f9032c09b271
Francesco Lisotti - francesco.lisotti2@studio.unibo.it <francesco.lisotti2@studio.unibo.it>
2022-11-07 10:44:33 +0100

    Try

--- 61-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 61-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
-
+//try
 /**
  * A special utility class with methods that transform collections using {@link Function}s provided as parameters.
  */
